52af38bbfe095a7758d1915ce94f7872be729c76
Johanna Rizzi <rizzijohanna@gmail.com>
2022-11-04 14:27:35 +0100

    see country component

--- src/app/countries/pages/see-country/see-country.component.ts
+++ src/app/countries/pages/see-country/see-country.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Country } from './../../interfaces/countries.interfaces';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-see-country',
@@ -9,22 +11,23 @@ import { CountryService } from '../../services/country.service';
 })
 export class SeeCountyComponent implements OnInit {
 
+ country! : Country;
+
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private CountryService: CountryService
     ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params
-    .subscribe( ({id}) => {
-      console.log(id)
-      this.CountryService.getCountyForAlpha(id)
-      .subscribe(country => {
-        console.log(country);
-      })
 
-    })
-
+  this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.CountryService.getCountyForAlpha(id)),
+      tap(console.log)
+    )
+    .subscribe( country => this.country = country[0]);
+    
   }
 
 }
