ad477958e7727dbadaf23aef31baf875c221d359
Johanna Rizzi <rizzijohanna@gmail.com>
2022-11-10 13:25:34 +0100

    for region component

--- src/app/countries/pages/for-region/for-region.component.ts
+++ src/app/countries/pages/for-region/for-region.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-for-region',
   templateUrl: './for-region.component.html',
   styleUrls: ['./for-region.component.css']
 })
-export class ForRegionComponent implements OnInit {
+export class ForRegionComponent {
+
+  regions: string[] = ['africa', 'americas', 'asia', 'europa', 'oceania'];
+  regionActive: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  getClassCSS(region: string): string {
+    return (region === this.regionActive) ? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
+
+  activeRegion (region: string){
+    this.regionActive = region;
   }
 
 }
